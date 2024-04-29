c453868e8b76a61414f3b729c50acc7ea3cf5b1a
J-JackZhou <165709391+J-JackZhou@users.noreply.github.com>
2024-04-29 17:28:45 -0400

    Add files via upload

---
+++ TSX/Castle_Destroyed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Castle_Destroyed" tilewidth="64" tileheight="64" tilecount="20" columns="5">
+ <image source="../Main/Tiny Swords/Factions/Knights/Buildings/Castle/Castle_Destroyed.png" width="320" height="256"/>
+</tileset>
